3912d7cabcc7926ea72af3809b87b896ffb64270
lazyfirel <43263148+lazyfirel@users.noreply.github.com>
2019-01-16 07:09:54 +0300

    Update helloWorld2.java

--- src/helloWorld2.java
+++ src/helloWorld2.java
@@ -1,6 +1,8 @@
 public class helloWorld2 {
     public static void main(String[] args) {
         System.out.println("Second Commit");
+        
+        System.out.println("Changes on GitHub11");” 
     }
 
 }
